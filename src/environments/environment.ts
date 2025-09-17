a76a809de5e5279d6627ec3dd3180771317f2888
Den <saloed2206@gmail.com>
2025-09-17 09:59:53 +0200

    added env

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAQq5rWIzAVlI8LgbpKu_dKNSnBILclRek",
+    authDomain: "expence-tracker-fb.firebaseapp.com",
+    projectId: "expence-tracker-fb",
+    storageBucket: "expence-tracker-fb.firebasestorage.app",
+    messagingSenderId: "425086947523",
+    appId: "1:425086947523:web:f261098611c6febee05276",
+    measurementId: "G-1MV14QNF81"
+  }
+};
