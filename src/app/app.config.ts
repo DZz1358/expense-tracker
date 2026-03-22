83bbf52d1887d7857d294a495623f21893e70a18
Den <saloed2206@gmail.com>
2026-03-22 18:34:04 +0100

    remove unused code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { ErrorStateMatcher, MAT_DATE_LOCALE, provideNativeDateAdapter, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-
-import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 
@@ -15,8 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    // provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    // provideFirestore(() => getFirestore()),
     { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
     { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }
   ]
